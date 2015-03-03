a6800de40d11ba32a0ceb5348306d3f6b9996188
Stephane Collot <stephane.collot@gmail.com>
2015-03-03 10:33:30 -0500

    index for sarah

--- django/main/views.py
+++ django/main/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import Context, loader
+from django.shortcuts import render_to_response
 
 # Create your views here.
 
 def index(request):
-  return HttpResponse("Hello, world. Here is the index.")
+  return render_to_response('index.html')
+  #return HttpResponse("Hello, world. Here is the index.")
