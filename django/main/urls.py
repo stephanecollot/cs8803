ead318cb643a4e0cafe50ecc90d9fce0d6acbb9b
Stephane Collot <stephane.collot@gmail.com>
2015-03-03 10:45:43 -0500

    add of a static directory /s

--- django/main/urls.py
+++ django/main/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import patterns, url
 
 from main import views
+from django.conf import settings
 
 urlpatterns = patterns('',
   url(r'^$', views.index, name='index'),
 )
 
+urlpatterns += patterns('',
+  url(r'^s/(?P<path>.*)$', 'django.views.static.serve', {
+    'document_root': 'main/templates/s',
+  }),)
