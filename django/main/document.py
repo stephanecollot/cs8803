a67fd939dc3f2f2d9de5a51a4a3773a97ac0f75f
Stephane Collot <stephane.collot@gmail.com>
2015-03-05 10:39:48 -0500

    call for Jason code

--- django/main/document.py
+++ django/main/document.py
@@ -26,8 +26,18 @@ class DocumentList:
     self.docs.append(doc)
     #logger.info('Doc List Add')
     
-  def debugDocs(self):
+  def debug(self):
     logger.debug('Doc at 1:' + str(self.docs[1].id) + self.docs[1].fileName)
     for doc in self.docs:
       #logger.info('Doc Listing' + doc.content)
       pass
+      
+  def entityExtraction(self):
+    logger.info('entityExtraction +')
+    # Jason ;) you can start here, may be you can create a similar architecture of document.py but entity.py
+    # and may be this function can output an EntityList, then stored in GlobalLoad (singleton accesible everywhere)
+    for doc in self.docs:
+      #logger.info('Doc Listing' + doc.content)
+      pass
+      
+    logger.info('entityExtraction -')
--- django/main/global_load.py
+++ django/main/global_load.py
@@ -12,13 +12,18 @@ class GlobalLoad:
 
   def __init__(self):
     logger.info('GlobalLoad init ++++++++')
-         
+    
+    #Document load, online once
     directory = "../stego_dataset/"
     os.chdir(directory)
     for file in glob.glob("*.txt"):
       with open(directory+file, 'r') as content_file:
         self.docList.addDoc(content_file.read(), file)
+    self.docList.debug()
+        
+    #Entity extraction
+    self.docList.entityExtraction()
     
-    self.docList.debugDocs()
     logger.info('GlobalLoad init --------')
     self.ready = True
+
